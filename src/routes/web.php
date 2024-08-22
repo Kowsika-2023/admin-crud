cb0e0cbf9f5158366cb03121910c3fc3ae2099b5
edwardNinos <edward@ninositsolution.com>
2024-08-22 11:07:40 +0530

    status

--- src/routes/web.php
+++ src/routes/web.php
@@ -8,7 +8,7 @@ Route::get('list', [AdminController::class,'adminList'])->name('admin.list');
 Route::get('create', [AdminController::class,'create'])->name('admin.create');
 Route::post('store',[AdminController::class,'store'])->name('admin.store');
 Route::get('show',[AdminController::class,'show'])->name('admin.show');
-Route::get('status',[AdminController::class,'status'])->name('status');
+Route::get('status',[AdminController::class,'status'])->name('admin.status');
 
 
 
